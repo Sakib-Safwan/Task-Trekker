639ca92645aed75f37d704169fa7df03200f858f
Sakib <sakib.safwan@gmail.com>
2024-12-22 19:16:17 +0600

    Add project files.

---
+++ Time_Trekker/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Time_Trekker.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
